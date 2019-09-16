cbc8e66d1a56d3abf76e3fe87dbf19a34f734a90
minghost@qq.com <minghost@qq.com>
2019-09-16 18:04:23 +0800

    'the-fourth-commit'

---
+++ src/views/home/childComps/HomeFeature.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="home-feature">
+    <a href="https://act.mogujie.com/zzlx67">
+    <img src="~assets/img/home/recommend_bg.jpg" ></a>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+#home-feature img{
+  width: 100%;
+}
+</style>
