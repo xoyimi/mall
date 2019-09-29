940b0e1ecad8a2963a4f220983e3f19b831423e1
xoyimi <xoyimi@foxmail.com>
2019-09-29 23:20:53 +0800

    13day

---
+++ src/views/detail/child-comps/DetailBottomBar.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="detail-bottom-bar"></div>
+</template>
+
+<script>
+  export default {
+    name: "DetailBottomBar"
+  }
+</script>
+
+<style scoped>
+.detail-bottom-bar{
+  position: relative;
+  bottom: 0;
+  background-color: #ccc;
+}
+</style>
