f2a62ea4ecc9635f4439e8e5908d3fdd161c6535
xoyimi <xoyimi@foxmail.com>
2019-09-30 21:06:04 +0800

    13day

---
+++ src/views/cart/child-comps/CartBottom.vue
@@ -0,0 +1,13 @@
+<template>
+  $END$
+</template>
+
+<script>
+  export default {
+    name: "CartBottom"
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/cart/child-comps/Tick.vue
@@ -0,0 +1,13 @@
+<template>
+  $END$
+</template>
+
+<script>
+  export default {
+    name: "Tick"
+  }
+</script>
+
+<style scoped>
+
+</style>
