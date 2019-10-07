05a4a4e38ad8904cf46048764518c00d476eab3e
xoyimi <xoyimi@foxmail.com>
2019-10-07 22:40:56 +0800

    commit profile

---
+++ src/views/category/child-comps/CategoryLeft.vue
@@ -0,0 +1,13 @@
+<template>
+  $END$
+</template>
+
+<script>
+  export default {
+    name: "CategoryLeft"
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/category/child-comps/CategoryRight.vue
@@ -0,0 +1,13 @@
+<template>
+  $END$
+</template>
+
+<script>
+  export default {
+    name: "CategoryRight"
+  }
+</script>
+
+<style scoped>
+
+</style>
